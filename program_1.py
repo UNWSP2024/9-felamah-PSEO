5ce36e4247b04ebf4b5246cae965229b71eef1a8
felamah-PSEO <felamah@students.unwsp.edu>
2025-10-30 14:27:03 -0500

    Assignment #9

--- program_1.py
+++ program_1.py
@@ -1,12 +1,22 @@
+# Faith Lamah
+# 10/30/2025
 # Program #1: Item Counter
 # Assume a file containing a series of names (as strings) is named names.txt 
 # (Use the included example file names.txt) and exists on the computer's disk.
 # Write a program that displays the number of names that are stored in the file.
 
 def count_file_lines():
-    ######################
-    # Add your code here #
-    ######################
+
+    count = 0
+    names_file = open('names.txt','r')
+    for name in names_file:
+        count += 1
+
+    print(f'The number of names stored in the file is {count}.')
+
+    names_file.close()
+
+
     print('In the count_file_lines function')
 
 
--- program_2.py
+++ program_2.py
@@ -1,5 +1,27 @@
+# Faith Lamah
+# 10/30/2025
 # Program #2: Random Number File Writer
 # Write a program that writes a series of random numbers (up to 1000) to a file.
 # Each random number should be in the range of 1 through 500. 
 # The application should let the user specify how many random numbers the file will hold 
 # (up to 1000).
+
+import random
+def random_number_file():
+
+    numberRandoms = int(input('Enter the number of random numbers the file will hold (Max of 1000):'))
+
+    if numberRandoms > 1 and numberRandoms < 1000:
+        number_file = open('random_numbers.txt', 'w')
+        for i in range(1, numberRandoms + 1):
+            random_number = random.randint(1, 500)
+            number_file.write(str(random_number) + '\n')
+
+        print('In the random_number_file.')
+    else:
+        print('Enter a number between 1 and 1000')
+
+
+    number_file.close()
+
+random_number_file()
